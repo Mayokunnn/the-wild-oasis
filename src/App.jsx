5bbcdf2cef4e8a143b26b44b66a7ce208d0482be
Mayokunnn <kareola960@gmail.com>
2024-01-15 22:56:31 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                size="medium"
+                variation="secondary"
+                onClick={() => alert("Checked in")}
+              >
+                Check In
+              </Button>
+              <Button
+                size="medium"
+                variation="primary"
+                onClick={() => alert("Checked out")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Forms</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
---
+++ src/ui/Row.jsx
@@ -0,0 +1,26 @@
+import styled, { css } from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+
+  ${({ type }) =>
+    type === "horizontal" &&
+    css`
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    `}
+
+  ${({ type }) =>
+    type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+`;
+
+Row.defaultProps = {
+  type: "vertical",
+};
+
+export default Row;
